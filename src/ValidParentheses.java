487d9f37328a627942579e5af9a12073da787606
Maxim Dzalba <brest-dzalba@list.ru>
2024-03-27 19:01:06 +0300

    tasks from leetcode

---
+++ src/ValidParentheses.java
@@ -0,0 +1,39 @@
+
+//Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+//
+//An input string is valid if:
+//
+//Open brackets must be closed by the same type of brackets.
+//Open brackets must be closed in the correct order.
+//Every close bracket has a corresponding open bracket of the same type.
+
+import java.util.Stack;
+
+public class ValidParentheses {
+
+    public static boolean isValid(String s) {
+        char[] brackets = s.toCharArray();
+        Stack<Character> stack = new Stack<>();
+        for (char bracket : brackets) {
+            if (bracket == '(' || bracket == '{' || bracket == '[') {
+                stack.push(bracket);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                } else if ((bracket == '}' && stack.peek() != '{') || (bracket == ']' && stack.peek() != '[')
+                        || (bracket == ')' && stack.peek() != '(')) {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isValid("()"));
+        System.out.println(isValid("()[]{}"));
+        System.out.println(isValid("(]"));
+    }
+}
