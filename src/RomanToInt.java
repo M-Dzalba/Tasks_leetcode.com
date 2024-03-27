9db149805fbdcee77ca323bc9bbe144881ac241e
Maxim Dzalba <brest-dzalba@list.ru>
2024-03-27 17:26:12 +0300

    tasks from leetcode

--- src/RomanToInt.java
+++ src/RomanToInt.java
@@ -8,6 +8,7 @@
 //C             100
 //D             500
 //M             1000
+
 //Input: s = "LVIII"
 //Output: 58
 //Explanation: L = 50, V= 5, III = 3.
@@ -15,10 +16,43 @@
 public class RomanToInt {
 
     public static int romanToInt(String s) {
-        int res=0;
-
-
-        return res;
+        int result=0;
+        char[]chars=s.toCharArray();
+        int symToVal;
+        for (char aChar : chars) {
+            switch (aChar) {
+                case 'I' -> {
+                    symToVal = 1;
+                    result += symToVal;
+                }
+                case 'V' -> {
+                    symToVal = 5;
+                    result += symToVal;
+                }
+                case 'X' -> {
+                    symToVal = 10;
+                    result += symToVal;
+                }
+                case 'L' -> {
+                    symToVal = 50;
+                    result += symToVal;
+                }
+                case 'C' -> {
+                    symToVal = 100;
+                    result += symToVal;
+                }
+                case 'D' -> {
+                    symToVal = 500;
+                    result += symToVal;
+                }
+                case 'M' -> {
+                    symToVal = 1000;
+                    result += symToVal;
+                }
+                default -> System.out.println("invalid symbol");
+            }
+        }
+        return result;
     }
     public static void main(String[] args) {
 
