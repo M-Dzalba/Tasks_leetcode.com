a1bbc6e8b222c2706650f3598364505b2c39c70d
Maxim Dzalba <brest-dzalba@list.ru>
2024-03-29 08:48:55 +0300

    tasks from leetcode

--- src/Sqrt_x.java
+++ src/Sqrt_x.java
@@ -17,32 +17,24 @@ public class Sqrt_x {
     public static int mySqrt(int x) {
         if (x == 0 || x == 1)
             return x;
-
-
         int start = 1;
         int end = x;
         int mid;
 
-
         while (start <= end) {
-
             mid = start + (end - start) / 2;
-
-
-            if ((long) mid * mid > (long) x)
+            if (mid * mid > x)
                 end = mid - 1;
             else if (mid * mid == x)
-
                 return mid;
             else
-
                 start = mid + 1;
         }
-
         return Math.round(end);
     }
 
     public static void main(String[] args) {
         System.out.println(mySqrt(4));
+        System.out.println(mySqrt(8));
     }
 }
