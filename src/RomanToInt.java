a38b513f6ad2632fc5e41b77d16950dc590ce086
Maxim Dzalba <brest-dzalba@list.ru>
2024-03-29 09:57:21 +0300

    tasks from leetcode

--- src/RomanToInt.java
+++ src/RomanToInt.java
@@ -14,47 +14,49 @@
 //Explanation: L = 50, V= 5, III = 3.
 
 public class RomanToInt {
+    public static int NumValue(char rom) {
+        if (rom == 'I')
+            return 1;
+        if (rom == 'V')
+            return 5;
+        if (rom == 'X')
+            return 10;
+        if (rom == 'L')
+            return 50;
+        if (rom == 'C')
+            return 100;
+        if (rom == 'D')
+            return 500;
+        if (rom == 'M')
+            return 1000;
+        return -1;
+    }
 
     public static int romanToInt(String s) {
-        int result=0;
-        int symToVal;
-        for (int i=0;i<s.length();i++) {
-            switch (s.charAt(i)) {
-                case 'I' -> {
-                    symToVal = 1;
-                    result += symToVal;
+        int sum = 0;
+        for (int i=0; i<s.length(); i++) {
+            int s1 = NumValue(s.charAt(i));
+            if (i+1 <s.length()) {
+                int s2 = NumValue(s.charAt(i+1));
+                if (s1 >= s2) {
+                    sum = sum + s1;
                 }
-                case 'V' -> {
-                    symToVal = 5;
-                    result += symToVal;
+                else{
+                    sum = sum - s1;
                 }
-                case 'X' -> {
-                    symToVal = 10;
-                    result += symToVal;
-                }
-                case 'L' -> {
-                    symToVal = 50;
-                    result += symToVal;
-                }
-                case 'C' -> {
-                    symToVal = 100;
-                    result += symToVal;
-                }
-                case 'D' -> {
-                    symToVal = 500;
-                    result += symToVal;
-                }
-                case 'M' -> {
-                    symToVal = 1000;
-                    result += symToVal;
-                }
-                default -> System.out.println("invalid symbol");
+            }
+            else {
+                sum = sum + s1;
             }
         }
-        return result;
+        return sum;
+
+
     }
     public static void main(String[] args) {
 
+        System.out.println(romanToInt("III"));
         System.out.println(romanToInt("LVIII"));
+        System.out.println(romanToInt("MCMXCIV"));
     }
 }
